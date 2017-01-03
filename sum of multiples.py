5f8561cfd5a676e348aa6a2c3338c7b43a25f516
Joshua Nelson <jyn514@users.noreply.github.com>
2017-01-03 03:01:02 -0500

    fixed remainder operation

--- sum of multiples.py
+++ sum of multiples.py
@@ -2,15 +2,15 @@
 sum=0
 
 for x in range (0,1000):
-    if x%%3==0:
+    if x%3==0:
         sum+=x
         
 for x in range (0,1000):
-    if x%%5==0:
+    if x%5==0:
         sum+=x
         
 for x in range (0, 1000):
-    if x%%15==0:
+    if x%15==0:
         sum-=x
         
 print(sum)
