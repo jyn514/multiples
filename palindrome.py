cc231b27d57d503fe8237f4d8febce977427ecb3
Joshua Nelson <jyn514@users.noreply.github.com>
2017-01-03 10:24:40 -0500

    now function
    creates a list and uses .reverse() method
    created variable 'num' which is reversed number

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,15 @@
 palindromes=[]
+sequence=[]
 
 def palindrome(x):
     for n in range (11, x):
-        if int(string(list(sort(list(string(x))), reversed = true))) == x:
-            palindromes.append(x)
-            
+        sequence = list(str(n))
+        sequence.reverse()
+        num=""
+        for x in sequence:
+            num+=str(x)
+        if n == int(num):
+            palindromes.append(n)
+    print(palindromes)
+
+palindrome(999999)
