112a177b84611f70f8b73efae7a33ef8868359ee
Joshua Nelson <jyn514@users.noreply.github.com>
2017-01-05 10:07:20 -0500

    prints palindromes with both factors under 1000

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,6 @@
-
-
-
+match=[]
 
 def palindrome(x):
-    match=[]
     sequence=[]
     for n in range (x, 10, -1):
         sequence = list(str(n))
@@ -22,8 +19,14 @@ def factor(n):
     for x in range(2, int(n**.5)+1):
         if n%x == 0:
             factors.append(x)
-    print("Factors:")
-    print(factors)
+    return factors
 
 greatest=palindrome(999999)
+#change palindrome(x) to use a different limit
 factor(greatest)
+
+for x in match:
+    for n in factor(x):
+        if x/n < 1000:
+            print (x, n)
+        
