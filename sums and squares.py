1ed6c75e3b3294a9a1ee7b2610e1feb655e715a6
Joshua Nelson <jyn514@users.noreply.github.com>
2017-01-19 21:09:27 -0500

    Create sums and squares.py

---
+++ sums and squares.py
@@ -0,0 +1,18 @@
+def sum_squares (x):
+    sum = 0
+    for y in range(1, x+1):
+        sum += y**2
+    print(sum)
+    return(sum)
+
+def square_sums (x):
+    sum = 0
+    for y in range (1, x+1):
+        sum += y
+    print(sum**2)
+    return(sum**2)
+
+def sum_square_difference (x):
+    print(square_sums(x) - sum_squares(x))
+
+sum_square_difference (10)
