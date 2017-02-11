8c466c47bf90c067a35f603196b12600554459c0
Joshua Nelson <jyn514@users.noreply.github.com>
2017-02-11 17:05:29 -0500

    Update and rename is_prime.py to primes.py
    several functions relating to primes

---
+++ primes.py
@@ -0,0 +1,24 @@
+class prime:
+    def is_prime(num):
+        if all(num%y != 0 for y in range(2, num)):
+            return True
+        else:
+            return False
+
+    def next(num):
+        num+=1
+        while not (prime.is_prime(num)):
+            num+=1
+        return num
+
+    def previous(num):
+        num-=1
+        while not (prime.is_prime(num)):
+            num-=1
+        return num
+
+    def all_below(num):
+        for x in range(2,num):
+            if prime.is_prime(x):
+                print(x, end=" ")
+
