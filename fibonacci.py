b341f1ffeba56e0042390aeca4bb2f21e0fb810c
Joshua Nelson <jyn514@users.noreply.github.com>
2017-02-11 16:47:59 -0500

    Create fibonacci.py
    allows for keyword 'all', which prints every fibonacci number, not just the greatest.

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+def fibonacci(n, *option, **kwarg):
+    t,a,b=0,0,1
+    if ((kwarg=={}) and (option==())):
+        while  t<n:
+            a,b=b,a+b
+            t+=1
+        print(a)
+    elif ('all' in option):
+        while  t<n:
+            print(a)
+            a,b=b,a+b
+            t+=1
+        print(a)
+    else:
+        print("You entered an option that is not available.")
+    
